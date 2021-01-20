3f1cd2be85b3a921a388e162ff530bfed4324096
Ahnaf Fattah <ahnaf.fattah@outlook.com>
2021-01-20 14:40:40 +0700

    make code for put data

--- src/main/java/io/project/mock/service/customersService.java
+++ src/main/java/io/project/mock/service/customersService.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Consumes;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -49,4 +50,13 @@ public class customersService {
     public void addData(customers cs) throws Exception{
         br.addData(cs);
     }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public customers editCustomers(customers cs){
+        br.editData(cs);
+        return cs;
+    }
+    
+   
 }   
